293f155daf5442f5042b61691ebe8cc12086e368
Ashley Ho <60860374+ashleylho@users.noreply.github.com>
2022-12-15 19:04:05 -0800

    Time: 76 ms, Memory: 46.6 MB - LeetHub

---
+++ single-number/single-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let map = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (map[nums[i]]) {
+            delete map[nums[i]]
+        } else {
+            map[nums[i]] = 1
+        }
+    }
+    return Object.keys(map)[0]
+};
